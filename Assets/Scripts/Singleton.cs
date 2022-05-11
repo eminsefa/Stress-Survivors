4d08f7e3baa419fe180e45e8c614e74a50876617
Deithl <kerimfidan01@gmail.com>
2022-05-11 23:28:39 +0300

    Update Singleton.cs

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 
 namespace StressSurvivors
 {
-    public class Singleton<T> : MonoBehaviour
-        where T : Component
+    public abstract class Singleton<T> : MonoBehaviour
+        where T : Singleton<T>
     {
-        public static T Instance { get; private set; }
+        static T _instance;
 
-        public virtual void Awake()
+        public static T Instance
         {
-            if (Instance == null)
+            get
             {
-                Instance = this as T;
-                DontDestroyOnLoad(this);
-            }
-            else
-            {
-                Destroy(gameObject);
+                if (_instance) return _instance;
+
+#if UNITY_EDITOR
+                var instances = FindObjectsOfType<T>();
+
+                if (instances.Length == 0)
+                    return default;
+
+                if (instances.Length > 1)
+                    for (var i = instances.Length - 1; i >= 1; i--)
+                    {
+                        var o = instances[i];
+                        Debug.LogWarning($"There are multiple instances in the scene. Destroying {o.name}", o);
+                        Destroy(o);
+                    }
+
+                _instance = instances[0];
+#else
+                _instance = FindObjectOfType<T>();
+#endif
+
+                return _instance;
             }
         }
     }
